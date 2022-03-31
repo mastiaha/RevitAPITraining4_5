d17fc8b89bb86b96f41b3222122352ecc4de8f5a
Yovanovich.aa <Yovanovich.aa@DESKTOP-ITANAVJ>
2022-03-31 19:08:27 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining45/Main.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining45
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string roomInfo = string.Empty;
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+            string exelPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "rooms.xlsx");
+
+            using (FileStream stream = new FileStream(exelPath, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("Лист1");
+
+                int rowIndex = 0;
+                foreach (var room in rooms)
+                {
+                    sheet.SetCellValue(rowIndex, columnIndex: 0, room.Name);
+                    sheet.SetCellValue(rowIndex, columnIndex: 1, room.Number);
+                    sheet.SetCellValue(rowIndex, columnIndex: 2, room.Area);
+                    rowIndex++;
+                }
+                workbook.Write(stream);
+                workbook.Close();
+            }
+            System.Diagnostics.Process.Start(exelPath);
+
+         return Result.Succeeded;
+        }
+    }
+
+}
